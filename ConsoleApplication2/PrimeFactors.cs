ee36a28600bdc9f8216092057d59e28196e98d1c
Rebecca Sliter <sliterrm@gmail.com>
2013-08-29 15:28:26 -0700

    first commit. prime factor generator works.

---
+++ ConsoleApplication2/PrimeFactors.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactorsProject
+{
+    public class PrimeFactors
+    {
+        public static void Main() { 
+        
+        }
+
+        public static List<int> Generate(int rootNumber)
+        {
+            List<int> factors = new List<int>();
+            int valueUpToRootNumber = 2;
+
+            while (rootNumber > 1)
+            {
+                while (rootNumber % valueUpToRootNumber == 0)
+                {
+                    factors.Add(valueUpToRootNumber);
+                    rootNumber = rootNumber / valueUpToRootNumber;
+                }
+                valueUpToRootNumber++;
+            }
+            if (rootNumber > 1)
+            {
+                factors.Add(rootNumber);
+            }
+            return factors;
+        }
+    }
+}
---
+++ UnitTestProject1/PrimeFactorsTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using PrimeFactorsProject;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class PrimeFactorsTest
+    {
+        [Test]
+        public void TestGenerateYieldsEmptyList()
+        {
+            List<int> expected = new List<int>();
+            Assert.AreEqual(expected, PrimeFactors.Generate(1));
+        }
+
+        [Test]
+        public void TestGenerateWithTwoYieldsTwo() {
+            List<int> expected = new List<int>();
+            expected.Add(2);
+            Assert.AreEqual(expected, PrimeFactors.Generate(2));
+        }
+
+        [Test]
+        public void TestGenerateWithFourYieldsTwoAndTwo()
+        {
+            List<int> expected = new List<int>();
+            expected.Add(2);
+            expected.Add(2);
+            Assert.AreEqual(expected, PrimeFactors.Generate(4));
+        }
+
+        [Test]
+        public void TestGenerateWithSixYieldsTwoAndThree()
+        {
+            List<int> expected = new List<int>();
+            expected.Add(2);
+            expected.Add(3);
+            Assert.AreEqual(expected, PrimeFactors.Generate(6));
+        }
+
+        [Test]
+        public void TestGenerateWithEightYieldsTwoAndTwoAndTwo()
+        {
+            List<int> expected = new List<int>();
+            expected.Add(2);
+            expected.Add(2);
+            expected.Add(2);
+            Assert.AreEqual(expected, PrimeFactors.Generate(8));
+        }
+
+        [Test]
+        public void TestGenerateWithNineYieldsThreeAndThree()
+        {
+            List<int> expected = new List<int>();
+            expected.Add(3);
+            expected.Add(3);
+            Assert.AreEqual(expected, PrimeFactors.Generate(9));
+        }
+        
+        [Test]
+        public void TestGenerateWithTenYieldsFiveAndTwo()
+        {
+            List<int> expected = new List<int>();
+            expected.Add(2);
+            expected.Add(5);
+            Assert.AreEqual(expected, PrimeFactors.Generate(10));
+        }
+    }
+}
